25831f572e698f0095ab81d68bf2df0c20e7880d
Chandani Jayantilal Solanki <solanki.chandani14@gmail.com>
2025-08-02 23:24:16 -0400

    Added test for new activity numeric input

--- app/src/androidTest/java/com/example/InstrumentalTesting/ChangeTextBehaviorKtTest.kt
+++ app/src/androidTest/java/com/example/InstrumentalTesting/ChangeTextBehaviorKtTest.kt
@@ -47,4 +47,12 @@ class ChangeTextBehaviorKtTest {
         onView(withId(R.id.changeTextBt)).perform(click())
         onView(withId(R.id.textToBeChanged)).check(matches(withText("123")))
     }
+    @Test
+    fun changeText_newActivity_with123() {
+        onView(withId(R.id.editTextUserInput))
+            .perform(typeText("123"), closeSoftKeyboard())
+        onView(withId(R.id.activityChangeTextBtn)).perform(click())
+        onView(withId(R.id.show_text_view)).check(matches(withText("123")))
+    }
+
 }
