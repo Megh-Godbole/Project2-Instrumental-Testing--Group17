b8613d7aa029f55eb687660a87d61c3bd820b103
Chandani Jayantilal Solanki <solanki.chandani14@gmail.com>
2025-08-02 23:11:15 -0400

    Added Instrumental Testing files

---
+++ app/src/androidTest/java/com/example/InstrumentalTesting/ChangeTextBehaviorKtTest.kt
@@ -0,0 +1,27 @@
+package com.example.InstrumentalTesting
+
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.InstumentalTesting.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ChangeTextBehaviorKtTest {
+
+    @get:Rule
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    companion object {
+        private const val STRING_TO_BE_TYPED = "Espresso"
+    }
+
+
+}
